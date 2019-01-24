bef493b957135094429a63e8446414e9fd7952d0
wangzhaohui <alex.wang@zving.com>
2019-01-24 15:37:02 +0800

    更新zving二次开发命名空间管理插件

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ export default zving.dirname.filename = obj
 let visitor = {
   // export default 语句解析时触发该函数
   ExportDefaultDeclaration(path, ref = { opts: {} }) {  // path 语句抽象语法树 opts 插件参数
-    const filePath = this.file.opts.sourceFileName.replace(/\\/g, '/')
-    const include = new RegExp(ref.opts.include || 'src/(common|components|router|views)/')
+    const fileOpts = this.file.opts
+    const filePath = (fileOpts.sourceFileName || fileOpts.filename).replace(/\\/g, '/')
+    const include = new RegExp(ref.opts.include || 'src/\\w+/')
     if (!include.test(filePath)) {
       return
     }
     let ns = ref.opts.namespace || 'zving'
     let modulePath = filePath.substr(filePath.indexOf('src/') + 4).replace(/\.\w+$/, '')
+    // 特殊符号要改，如 components.nw-corner-button 转成 components.nwCornerButton
     modulePath = modulePath.replace(/[^\/\.\w]+(\w)/g, (a, b, c, d) => {
       return b.toUpperCase()
     })
@@ -32,14 +34,26 @@ let visitor = {
       let callExpression = types.expressionStatement(
         types.callExpression(
           types.identifier('namespace'),
-          [types.stringLiteral(moduleRefer)]
+          [types.stringLiteral(moduleRefer.replace(/\.\w+$/, ''))]
         )
       )
+      // 1.0.3 20180806
+      //let exportDeclaration = types.exportDefaultDeclaration(
+      //  types.assignmentExpression(
+      //    '=',
+      //    buildMemberExprssion(moduleRefer),
+      //    types.cloneDeep(declaration)
+      //  )
+      //)
+      // ^1.1.0 20190123
       let exportDeclaration = types.exportDefaultDeclaration(
         types.assignmentExpression(
           '=',
           buildMemberExprssion(moduleRefer),
-          types.cloneDeep(declaration)
+          types.callExpression(
+            types.memberExpression(types.identifier('Object'), types.identifier('assignIf'), false),
+            [buildMemberExprssion(moduleRefer), types.cloneDeep(declaration)]
+          )
         )
       )
       // if(moduleRefer == 'zving.components.Position')
